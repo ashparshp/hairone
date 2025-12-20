fdb26a5e72acbc67b6ab7fc2284318e295ddfec8
Ashparshp <ashparsh.connects@gmail.com>
2025-12-20 17:47:55 +0530

    fix: Update DigitalOcean Spaces region to 'blr1'

--- server/src/routes/shopRoutes.js
+++ server/src/routes/shopRoutes.js
@@ -22,7 +22,7 @@ const {
 // 1. DigitalOcean Spaces Configuration from ENV
 const s3 = new S3Client({
     endpoint: process.env.DO_SPACES_ENDPOINT,
-    region: "us-east-1", 
+    region: "blr1", 
     credentials: {
         accessKeyId: process.env.DO_SPACES_KEY,
         secretAccessKey: process.env.DO_SPACES_SECRET,
